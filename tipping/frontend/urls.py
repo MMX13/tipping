83224fdfb7ee778ed68944eea4a9bbeef740358f
Ben <bgaramy@hotmail.com>
2016-02-07 11:14:49 +1000

    starting auth

--- tipping/frontend/urls.py
+++ tipping/frontend/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    url('^', views.base)
+    url('^accounts/', include('registration.backends.simple.urls')),
+    #url('^login', auth_views.login, name="login"),
+    url('^$', views.base)
 ]
--- tipping/tipping/settings.py
+++ tipping/tipping/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'frontend.apps.FrontendConfig',
-    'api.apps.ApiConfig'
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_URL = '/login'
