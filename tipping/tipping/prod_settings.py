4608ef73fc4415b1712a9ac4454bb2d5296b9c62
Ben <bgaramy@hotmail.com>
2016-03-01 19:36:07 +1000

    Restricting to heroku

--- tipping/tipping/prod_settings.py
+++ tipping/tipping/prod_settings.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 SECRET_KEY = os.environ['SECRET_KEY']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 DEBUG=True
 
