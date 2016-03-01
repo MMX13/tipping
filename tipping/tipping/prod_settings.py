e0a7c85d7130d138b874955f4744ee1768712954
Ben <bgaramy@hotmail.com>
2016-03-01 21:41:55 +1000

    Debug off

--- tipping/tipping/prod_settings.py
+++ tipping/tipping/prod_settings.py
@@ -6,8 +6,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = ['.herokuapp.com']
 
-DEBUG=True
-
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
