888646f6f1038e015d5c2876b35f8c74ab6431dd
Ben <bgaramy@hotmail.com>
2018-02-26 21:49:45 +1000

    Extract default settings from prod_settings

--- tipping/tipping/prod_settings.py
+++ tipping/tipping/prod_settings.py
@@ -1,12 +1,11 @@
-import os
-import dj_database_url
+from base import *
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+DEBUG = os.environ.get('DEBUG', False)
 
 SECRET_KEY = os.environ['SECRET_KEY']
+
 ALLOWED_HOSTS = ['.herokuapp.com']
-DEBUG = os.environ.get('DEBUG', False)
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -22,49 +21,6 @@ LOGGING = {
         },
     },
 }
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'frontend.apps.FrontendConfig',
-    'api.apps.ApiConfig',
-    'rest_framework'
-]
-
-MIDDLEWARE_CLASSES = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'tipping.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'tipping.wsgi.application'
 
 DATABASES = {
     'default': {
@@ -91,16 +47,3 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
-
-LOGIN_URL = '/accounts/login/'
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'frontend/static'),
-)
